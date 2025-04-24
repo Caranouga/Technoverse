f1162a035d036968e3cdbfa4f8b61bb7d843888e
Caranouga <malonepayet@gmail.com>
2025-04-24 13:12:12 +0200

    Added QOL to ModRecipesProvider.java

--- src/main/java/fr/caranouga/technoverse/datagen/providers/ModRecipesProvider.java
+++ src/main/java/fr/caranouga/technoverse/datagen/providers/ModRecipesProvider.java
@@ -7,6 +7,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -25,12 +26,7 @@ public class ModRecipesProvider extends RecipeProvider implements IConditionBuil
     }
 
     private void storageBlock(Block block, Item item, RecipeOutput pRecipeOutput) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, block)
-                .pattern("AAA")
-                .pattern("AAA")
-                .pattern("AAA")
-                .define('A', item)
-                .unlockedBy(getHasName(item), has(item))
+        nineByNine(item, block)
                 .save(pRecipeOutput, Technoverse.MODID + ":" + getItemName(block) + "_from_" + getItemName(item));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, item, 9)
@@ -40,17 +36,21 @@ public class ModRecipesProvider extends RecipeProvider implements IConditionBuil
     }
 
     private void nuggetsToIngot(Item nugget, Item ingot, RecipeOutput pRecipeOutput) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ingot)
-                .pattern("AAA")
-                .pattern("AAA")
-                .pattern("AAA")
-                .define('A', nugget)
-                .unlockedBy(getHasName(nugget), has(nugget))
+        nineByNine(nugget, ingot)
                 .save(pRecipeOutput, Technoverse.MODID + ":" + getItemName(ingot) + "_from_" + getItemName(nugget));
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, nugget, 9)
                 .requires(ingot)
                 .unlockedBy(getHasName(ingot), has(ingot))
                 .save(pRecipeOutput, Technoverse.MODID + ":" + getItemName(nugget) + "_from_" + getItemName(ingot));
+    }
+
+    private ShapedRecipeBuilder nineByNine(ItemLike nine, ItemLike output) {
+        return ShapedRecipeBuilder.shaped(RecipeCategory.MISC, output)
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', nine)
+                .unlockedBy(getHasName(nine), has(nine));
     }
 }
